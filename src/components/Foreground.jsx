2d91e0cf3a09011721f6b59ab8d6c43edb094230
Praveen Kumar Chauhan <praveenkumarchauhan2022@gmail.com>
2024-01-15 17:23:15 +0530

    final commit

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import Card from './Card';
+import React, { useRef } from "react";
+import Card from "./Card";
 
 const Foreground = () => {
-  return <div className="fixed top-0 left-0 z-[3] w-full h-full">
-    <Card/>
-  </div>;
-}
+    const ref =useRef(null);
+  const data = [
+    {
+      desc: "This is Praveen Kumar Chauhan",
+      filesize: "0.9mb",
+      close: true,
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: " Praveen Kumar Chauhan",
+      filesize: "0.9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: "Naveen Chauhan",
+      filesize: "0.9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download", tagColor: "green" },
+    },
+    {
+      desc: "Sriniwas Chauhan",
+      filesize: "0.9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "The King Kohli",
+      filesize: "0.9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+  ];
+  return (
+    <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5">
+      {data.map((item,index)=>(
+          <Card data={item} reference={ref}/>
+      ))}
+    </div>
+  );
+};
 
-export default Foreground
+export default Foreground;
